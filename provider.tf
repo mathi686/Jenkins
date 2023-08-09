08fe08acd42b6f3ec14ab6a13c7fedac1cbaff4a
mathi686 <mathi686@gmail.com>
2023-08-09 12:33:01 +0530

    hg

--- provider.tf
+++ provider.tf
@@ -5,13 +5,13 @@ provider "aws" {
 }
 
 
-#terraform {
-#  backend "s3" {
-#    bucket = "ookey1"
-#    key = "mathi/terraform.tfstate"
-#    region = "us-east-2"
-#    access_key = "AKIAZN3BIDBBPUW7SOPI"
-#    secret_key = "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"
-#    dynamodb_table = "ookey"
-#  }
-#}:
+terraform {
+  backend "s3" {
+    bucket = "ookey1"
+    key = "mathi/terraform.tfstate"
+    region = "us-east-2"
+    access_key = "AKIAWNHN56SPM6TBQIPM"
+  secret_key = "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
+    dynamodb_table = "ookey"
+  }
+}
