6b7537281ea33d272d1853d3a9330af22c46cf41
mathi686 <mathi686@gmail.com>
2023-09-08 14:57:46 +0530

    gf

--- provider.tf
+++ provider.tf
@@ -1,17 +1,23 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "AKIAWNHN56SPM6TBQIPM"
-  secret_key = "oc9ydsNUj0z+rIpAeTYaDW7pGMwZlr2CFQh9urXs"
+  region = "us-east-2"  # Replace with your desired AWS region
+}
+
+data "aws_secretsmanager_secret" "my_secret" {
+  name = "newsec"  # Specify the name of your secret in AWS Secrets Manager
+}
+
+data "aws_secretsmanager_secret_version" "my_secret_version" {
+  secret_id = data.aws_secretsmanager_secret.my_secret.id
 }
 
 
-terraform {
-  backend "s3" {
-    bucket         = "ookey1"
-    key            = "mathi/terraform.tfstate"
-    region         = "us-east-2"
-    access_key     = "AKIAWNHN56SPM6TBQIPM"
-    secret_key     = "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"
-    dynamodb_table = "ookey"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "ookey1"
+#    key            = "mathi/terraform.tfstate"
+#    region         = "us-east-2"
+#    access_key     = "AKIAWNHN56SPM6TBQIPM"
+#    secret_key     = "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"
+#    dynamodb_table = "ookey"
+#  }
+#}
